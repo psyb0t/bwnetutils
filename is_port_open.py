d5d807f5c7c23b3db9a7095fd00904fad5fc8933
Ciprian Mandache <mandacheciprian@ymail.com>
2016-12-13 15:03:12 +0200

    We're going through changes

--- is_port_open.py
+++ is_port_open.py
@@ -1,5 +1,5 @@
 from random import choice
-from scapy.all import *
+from scapy.all import IP, TCP, sr1
 
 
 def is_port_open(ip_addr, port, timeout=5, verbose=False):
@@ -7,7 +7,7 @@ def is_port_open(ip_addr, port, timeout=5, verbose=False):
     ip.dst = ip_addr
 
     syn = TCP()
-    syn.sport = choice(range(1000, 9999))
+    syn.sport = choice(range(1000, 50000))
     syn.dport = port
     syn.flags = 'S'
     syn.seq = 1
--- is_proxy.py
+++ is_proxy.py
@@ -1,38 +1,39 @@
 import socket
 
 
-def is_proxy(ip, port):
+def is_proxy(ip, port, with_auth=True):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(2)
 
     try:
         client.connect((ip, port))
+
+        client.send('''GET http://example.org/ HTTP/1.1
+Server: example.org:80
+Host: example.org
+
+''')
+        recv_data = ''
+        buf = client.recv(10)
+        while buf:
+            recv_data += buf
+
+            http_response = recv_data.split('\r\n')
+            if len(http_response) > 1:
+                if 'HTTP/' not in http_response[0]:
+                    client.close()
+                    return False
+
+                if 'Proxy Authentication' in http_response[0]:
+                    client.close()
+                    return with_auth
+
+                expected_string = 'http://www.iana.org/domains/example'
+                if expected_string in ''.join(http_response):
+                    client.close()
+                    return True
+
+            buf = client.recv(100)
     except:
         client.close()
         return False
-
-    client.send('''GET http://bot.whatismyipaddress.com/ HTTP/1.1
-Server: bot.whatismyipaddress.com:80
-Host: bot.whatismyipaddress.com
-
-''')
-
-    recv_data = ''
-    buf = client.recv(10)
-    while True:
-        recv_data += buf
-
-        http_response = recv_data.split('\r\n')
-        if len(http_response) > 1:
-            if 'HTTP/' not in http_headers[0]:
-                client.close()
-                return False
-
-            if http_response[0] == 'HTTP/1.1 200 OK':
-                client.close()
-                return True
-
-            if 'Proxy Authentication' in http_response[0]:
-                client.close()
-                return True
-
-        buf = client.recv(100)
